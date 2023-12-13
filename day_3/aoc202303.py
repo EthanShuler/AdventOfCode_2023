e951996b80977f138b2e7d80b7f38a8c6cd6d95d
EthanShuler <13ethanshuler@gmail.com>
2023-12-13 14:01:31 -0800

    finished day 3

--- day_3/aoc202303.py
+++ day_3/aoc202303.py
@@ -9,16 +9,69 @@ pretty.install()
 
 def parse(puzzle_input):
     rows = puzzle_input.split('\n')
-    return rows
+    rows.append('.'*len(rows[0]))
+    rows.insert(0, '.'*len(rows[0]))
+    output = []
+    for line in rows:
+        line = '.' + line + '.'
+        output.append(line)
+    return output
 
 def part1(data):
     sum = 0
-   
+    for rowNum, line in enumerate(data):       
+        for match in re.finditer(r'\d+', line):
+            s = match.start()
+            e = match.end()
+            # look above
+            if re.search(r'[^\d\.]', data[rowNum - 1][s-1:e+1]):
+                sum += int(line[s:e])
+                continue
+            # look below
+            if re.search(r'[^\d\.]', data[rowNum + 1][s-1:e+1]):
+                sum += int(line[s:e])
+                continue
+            # look left
+            if re.search(r'[^\d\.]', line[s-1:s]):
+                sum += int(line[s:e])
+                continue
+            # look right
+            if re.search(r'[^\d\.]', line[e:e+1]):
+                sum += int(line[s:e])
+                continue
     return sum
-            
 
 def part2(data):
-    return 0;
+    sum = 0
+    gears = {}
+    for positionX, line in enumerate(data):
+        for positionY, char in enumerate(line):
+            if char == '*':
+                gears[(positionX, positionY)] = []
+    for positionX, line in enumerate(data):
+        for match in re.finditer(r'\d+', line):
+            s = match.start()
+            e = match.end()
+            # look above
+            gearPos = data[positionX - 1].find('*', s-1, e+1) 
+            if (gearPos != -1 and ((positionX-1, gearPos) in gears)):
+                gears[(positionX-1, gearPos)].append(line[s:e])
+            # look below
+            gearPos = data[positionX + 1].find('*', s-1, e+1) 
+            if (gearPos != -1 and ((positionX+1, gearPos) in gears)):
+                gears[(positionX+1, gearPos)].append(line[s:e])
+            # look left
+            gearPos = line.find('*', s-1, s) 
+            if (gearPos != -1 and ((positionX, gearPos) in gears)):
+                gears[(positionX, gearPos)].append(line[s:e])
+            # look right
+            gearPos = line.find('*', e, e+1) 
+            if (gearPos != -1 and ((positionX, gearPos) in gears)):
+                gears[(positionX, gearPos)].append(line[s:e])
+    for gearValues in gears.values():
+        if len(gearValues) == 2:
+            sum += (int(gearValues[0]) * int(gearValues[1]))
+    return sum;
 
 def solve(puzzle_input):
     """Solve the puzzle for the given input."""
