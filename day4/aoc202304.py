adfa5c0417d7f11e312d1cfbb851e82608077196
EthanShuler <13ethanshuler@gmail.com>
2023-12-14 10:38:04 -0800

    day 4

--- day4/aoc202304.py
+++ day4/aoc202304.py
@@ -2,15 +2,49 @@
 
 import pathlib
 import sys
+import numpy as np
+from rich import print
+from rich import pretty
+pretty.install()
 
 def parse(puzzle_input):
-    """Parse input."""
+    rows = puzzle_input.split('\n')
+    data = []
+    for line in rows:
+        winningNumbers = line[line.find(':')+1:line.find("|")]
+        winningNumbers = winningNumbers.strip()
+        myNumbers = line[line.find('|')+1:]
+        myNumbers = myNumbers.strip()
+        data.append((winningNumbers, myNumbers))
+    return data
+
+def countMatches(card):
+    count = 0
+    myNums = card[1].split()
+    winningNums = card[0].split()
+    for myNum in myNums:
+        if myNum in winningNums:
+            count += 1
+    return count
 
 def part1(data):
-    """Solve part 1."""
+    sum = 0
+    for card in data:
+        count = countMatches(card)
+        if count != 0:
+            sum += 2**(count - 1)
+    return sum
 
 def part2(data):
-    """Solve part 2."""
+    cardInstances = np.ones(len(data), dtype=int)
+    
+    for index, card in enumerate(data):
+        count = countMatches(card)
+        for i in range(count):
+            cardInstances[index+i+1] += cardInstances[index]
+
+    total_scratchcards = sum(cardInstances)
+    return total_scratchcards
 
 def solve(puzzle_input):
     """Solve the puzzle for the given input."""
